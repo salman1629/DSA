f8924279c087eef631d0c6c81a9fe75ec922458a
Salman.S <salman.s@apollodart.com>
2024-10-17 11:36:58 +0530

    linked list constractor

--- LINKED_LIST/LinkedList.java
+++ LINKED_LIST/LinkedList.java
@@ -1,3 +1,27 @@
 public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
 
+    /**
+     * inner class -- Node
+     */
+    public class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+
+        }
+
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+
+    }
 }
--- LINKED_LIST/Main.java
+++ LINKED_LIST/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+        
+    }
 }
