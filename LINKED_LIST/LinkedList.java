d7b6437a0e72703c15527694eb0c72534036f90f
Salman.S <salman.s@apollodart.com>
2024-10-18 09:58:21 +0530

    printList,getting head,tail,length,append,removeLast,prepend

--- LINKED_LIST/LinkedList.java
+++ LINKED_LIST/LinkedList.java
@@ -24,4 +24,62 @@ public class LinkedList {
         length = 1;
 
     }
+    public void printList() {
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }   
+    public void getHead(){
+        System.out.println("Head : " + head.value);
+    }
+    public void getTail(){
+        System.out.println("Tail : " + tail.value);
+    }
+    public void getLength(){
+        System.out.println("Length : " + length);
+    }
+
+    public void append(int value){
+        Node newNode = new Node (value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+    public Node removeLast(){
+        Node temp = head;
+        Node pre = head;
+        if(length == 0) return null;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if(length == 0){
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+
+        }
+        length++;
+
+    }
 }
