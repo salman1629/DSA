a16379dc16cfd6d16c6bcc57cb575bfe7622f424
Salman.S <salman.s@apollodart.com>
2024-10-24 11:06:14 +0530

    added remove method

--- LINKED_LIST/LinkedList.java
+++ LINKED_LIST/LinkedList.java
@@ -82,4 +82,36 @@ public class LinkedList {
         length++;
 
     }
+    public Node removeFirst(){
+        if(length == 0) return null;
+        Node temp = head;
+        head = temp.next;
+        temp.next = null;
+        length--;
+        if(length == 0){
+            tail = null;
+        }
+        return temp;
+    }
+    public Node get (int index){
+        if(index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public Node remove (int index) {
+        if(index < 0 || index >= length) return null;
+        if(index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node pre = get(index-1);
+        Node temp = pre.next;
+        pre.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+
+    }
 }
--- LINKED_LIST/Main.java
+++ LINKED_LIST/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
-        myLinkedList.append((2));
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+      
+       myLinkedList.append(5);
+       myLinkedList.append(6);
+
+        // System.out.println("removedFirst : " + myLinkedList.removeFirst().value);
         myLinkedList.printList();
+       
 
     }
 }
